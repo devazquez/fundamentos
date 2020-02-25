cdb31d257b85fcd99d83f75611490a4c257056e7
Dario E. Vázquez Ceballos <kienkane@gmail.com>
2020-02-25 14:24:25 -0600

    proyecto de para iniciar con html5, css, jquery en laravel en la asignatura de diseño de interfaces 20202, Facultad de Ciencias

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', function(){
+    return 'Hola desde la página de inicio';
+});
+
+
+Route::get('/contacto/{var}', function(){
+    dd(request()->var);
+    if(empty(request()->var))
+        return 'bienvenido a la página de contacto ';
+    return 'bienvenido a la página de contacto ' . request()->var;
+});
+
+Route::get('/foro', function(){
+    return 'Foro de la clase';
+});
